e54ee69bb9a2e65893bd1dce5cedee67175ff1e1
coderadi12345 <adityasharma74304@gmail.com>
2026-05-21 15:53:45 +0530

    changes made

--- src/server.ts
+++ src/server.ts
@@ -9,22 +9,12 @@ const app = express();
 
 app.use(express.json());
 
-/**
- * Registering all the routers and their corresponding routes with out app server object.
- */
-
 app.use(attachCorrelationIdMiddleware);
 app.use('/api/v1', v1Router);
 app.use('/api/v2', v2Router); 
 
-
-/**
- * Add the error handler middleware
- */
-
 app.use(appErrorHandler);
 app.use(genericErrorHandler);
-
 
 app.listen(serverConfig.PORT, () => {
     logger.info(`Server is running on http://localhost:${serverConfig.PORT}`);
